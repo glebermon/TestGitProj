aeb6eb93a0e699c04417c3c45df328ea31c2bd31
Gleber <monetchikov.gleb@icloud.com>
2022-10-30 12:17:19 +0600

    добавил лейбл по центру экрана в main

--- TestGitProj/ViewController.swift
+++ TestGitProj/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Hello World"
+        lbl.font = .boldSystemFont(ofSize: 50)
+        lbl.textAlignment = .center
+        return lbl
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(titleLabel)
+        titleLabel.frame = CGRect(x: 0, y: 0, width: view.frame.width - 20, height: 100)
+        titleLabel.center = view.center
     }
 
 
